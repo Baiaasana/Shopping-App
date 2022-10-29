af35a363566b91749e26ff83c3d130b81652962d
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-10-29 13:02:18 +0400

    navigation bug fixed

--- app/src/main/java/com/bendg/bg/presenter/ui/activity/MainActivity.kt
+++ app/src/main/java/com/bendg/bg/presenter/ui/activity/MainActivity.kt
@@ -1,14 +1,50 @@
 package com.bendg.bg.presenter.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.bendg.bg.R
+import com.bendg.bg.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment?
+        val navView = binding.bottomNavigationView
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.ordersFragment,
+                R.id.favoritesFragment,
+                R.id.profileFragment
+            )
+        )
+
+        if (navHostFragment != null) {
+            val navController = navHostFragment.navController
+            // Setup NavigationUI
+            setupActionBarWithNavController(navController,
+                appBarConfiguration)
+            navView.setupWithNavController(navController)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
