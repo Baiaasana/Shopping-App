e361ff7fc90118aa1c1b002ca77001eac58541d1
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-11-02 12:55:46 +0400

    userInfo/card design edited

--- app/src/main/java/com/bendg/bg/presenter/ui/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/bendg/bg/presenter/ui/fragment/profile/ProfileFragment.kt
@@ -1,15 +1,44 @@
 package com.bendg.bg.presenter.ui.fragment.profile
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.bendg.bg.common.BaseFragment
 import com.bendg.bg.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
+
+    private val viewModel: ProfileViewModel by viewModels()
     override fun listeners() {
+//        binding.ivChange.setOnClickListener {
+//
+//
+//
+//        }
     }
     override fun init() {
+        viewModel.showUserInfo()
     }
+
     override fun observers() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.userInfo.collect{
+                binding.apply {
+                    tvEmail.text = it.userInfo!!.email.toString()
+                    tvFirstName.text = it.userInfo.firstName.toString()
+                    tvLastName.text = it.userInfo.lastName.toString()
+                    tvUsername.text = it.userInfo.userName.toString()
+                    tvLocation.text = it.userInfo.location.toString()
+                    tvPhone.text = it.userInfo.phone_number.toString()
+                }
+            }
+        }
     }
+
+//    private fun changeObserve(){
+//
+//    }
+
 }
--- app/src/main/java/com/bendg/bg/utility/view_states/AuthenticationViewState.kt
+++ app/src/main/java/com/bendg/bg/utility/view_states/AuthenticationViewState.kt
@@ -1,5 +1,11 @@
 package com.bendg.bg.utility.view_states
 
+import com.bendg.bg.presenter.model.UserModel
+
 data class AuthenticationViewState(
     val message: String? = ""
+)
+
+data class UserInfoState(
+    val userInfo: UserModel? = UserModel()
 )
