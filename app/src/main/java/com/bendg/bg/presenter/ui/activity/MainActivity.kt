05f83a6aee2b1af04b13e131e99525ecadd92827
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-11-02 23:36:16 +0400

    settings/change password added

--- app/src/main/java/com/bendg/bg/presenter/ui/activity/MainActivity.kt
+++ app/src/main/java/com/bendg/bg/presenter/ui/activity/MainActivity.kt
@@ -2,16 +2,24 @@ package com.bendg.bg.presenter.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bendg.bg.R
+import com.bendg.bg.common.Constants
 import com.bendg.bg.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -54,6 +62,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, AuthActivity::class.java))
         }
     }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
