d4990f2a8cbbeabeb75b199ad1e1c1e4408ca84b
lomidzegaga <98084620+lomidzegaga@users.noreply.github.com>
2022-11-03 14:27:22 +0400

    implementing payment system

--- app/src/main/java/com/bendg/bg/presenter/ui/fragment/wallet/WalletFragment.kt
+++ app/src/main/java/com/bendg/bg/presenter/ui/fragment/wallet/WalletFragment.kt
@@ -1,5 +1,6 @@
 package com.bendg.bg.presenter.ui.fragment.wallet
 
+import androidx.fragment.app.viewModels
 import com.bendg.bg.common.BaseFragment
 import com.bendg.bg.databinding.FragmentWalletBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -7,13 +8,18 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class WalletFragment : BaseFragment<FragmentWalletBinding>(FragmentWalletBinding::inflate) {
 
-    override fun listeners() {
-    }
+    private val viewModel: WalletViewModel by viewModels()
+
+    override fun listeners() {}
 
     override fun init() {
+        viewModel.getMoney()
+        binding.tvPrice.text =
+            viewModel.updateMoney(binding.tvPrice.text.toString().toFloat()).toString()
+
+
     }
 
-    override fun observers() {
-    }
+    override fun observers() {}
 
 }
--- app/src/main/java/com/bendg/bg/presenter/ui/fragment/wallet/WalletViewModel.kt
+++ app/src/main/java/com/bendg/bg/presenter/ui/fragment/wallet/WalletViewModel.kt
@@ -1,6 +1,32 @@
 package com.bendg.bg.presenter.ui.fragment.wallet
 
 import androidx.lifecycle.ViewModel
+import com.bendg.bg.presenter.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class WalletViewModel : ViewModel() {
+    private val db: DatabaseReference =
+        FirebaseDatabase.getInstance().getReference("userInfo")
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    private val _newMoney = MutableStateFlow<UserModel>(UserModel())
+
+    fun getMoney() {
+        db.child(auth.currentUser?.uid!!).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(UserModel::class.java) ?: return
+                _newMoney.value = user
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
+    }
+
+    fun updateMoney(newMoney: Float) {
+        val balance = newMoney + _newMoney.value.cards!!.balance!!
+        db.child(auth.currentUser?.uid!!).child("cards").child("balance").setValue(balance)
+    }
 }
