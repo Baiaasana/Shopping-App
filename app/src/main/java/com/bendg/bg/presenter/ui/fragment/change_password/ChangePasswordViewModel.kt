b9871d522ccda96763c0544569b56822dfd55daf
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-11-04 17:15:33 +0400

    update bugs fixed

--- app/src/main/java/com/bendg/bg/presenter/ui/fragment/change_password/ChangePasswordViewModel.kt
+++ app/src/main/java/com/bendg/bg/presenter/ui/fragment/change_password/ChangePasswordViewModel.kt
@@ -1,64 +1,49 @@
 package com.bendg.bg.presenter.ui.fragment.change_password
 
 import androidx.lifecycle.ViewModel
-import com.bendg.bg.presenter.model.UserModel
+import androidx.lifecycle.viewModelScope
 import com.bendg.bg.utility.view_states.AuthenticationViewState
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class ChangePasswordViewModel : ViewModel() {
-
-//    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
-//    private val databaseReference: DatabaseReference =
-//        FirebaseDatabase.getInstance().getReference("userInfo")
-//
-//    private val _updateState = MutableStateFlow<AuthenticationViewState>(AuthenticationViewState())
-//    val updateState = _updateState.asStateFlow()
-//
-//    fun updatePassword(newPassword: String){
-//        auth.currentUser?.updatePassword(newPassword)?.addOnCompleteListener { task ->
-//            if(task.isSuccessful){
-//                databaseReference.child(auth.currentUser?.uid!!).child("password").setValue(newPassword)
-//                _updateState.value = _updateState.value.copy(message = "successsssss")
-//            }else{
-//                _updateState.value = _updateState.value.copy(message = "errrrorrr")
-//            }
-//        }
-//
-//    }
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private val databaseReference: DatabaseReference =
         FirebaseDatabase.getInstance().getReference("userInfo")
 
-    private val _updateState = MutableStateFlow<AuthenticationViewState>(AuthenticationViewState())
-    val updateState = _updateState.asStateFlow()
+    private val _updateState = MutableSharedFlow<AuthenticationViewState>()
+    val updateState = _updateState.asSharedFlow()
 
-    fun updatePassword(oldPass:String, newPassword: String){
+    fun updatePassword(oldPass: String, newPassword: String) {
 
-        databaseReference.child(auth.currentUser?.uid!!)
-            .addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    val userInformation = snapshot.getValue(UserModel::class.java) ?: return
-                    if(userInformation.password.toString() == oldPass){
-                        auth.currentUser?.updatePassword(newPassword)?.addOnCompleteListener { task ->
-                            if(task.isSuccessful){
-                                // an shemowmebis gareshe ganaaxle an databaseshi ar ganaaxlo
-                                databaseReference.child(auth.currentUser?.uid!!).child("password").setValue(newPassword).addOnCompleteListener {
-                                    _updateState.value = _updateState.value.copy(message = "Your password changed successfully!")
+        databaseReference.child(auth.currentUser?.uid!!).get().addOnSuccessListener {
+            if (it.exists()) {
+                val password = it.child("password").value
+                if (password.toString() == oldPass) {
+                    auth.currentUser?.updatePassword(newPassword)?.addOnCompleteListener { task ->
+                        viewModelScope.launch {
+                            if (task.isSuccessful) {
+                                databaseReference.child(auth.currentUser?.uid!!).child("password")
+                                    .setValue(newPassword)
+                                _updateState.emit(AuthenticationViewState(message = "success"))
+                            } else {
+                                viewModelScope.launch {
+                                    _updateState.emit(AuthenticationViewState(message = "Password did not changed, try later!"))
                                 }
-                            }else{
-                                _updateState.value = _updateState.value.copy(message = "Password did not changed, try later!")
                             }
                         }
-                    }else{
-                        _updateState.value = _updateState.value.copy(message = "Enter the correct password")
+                    }
+                } else {
+                    viewModelScope.launch {
+                        _updateState.emit(AuthenticationViewState(message = "old Password did not match"))
                     }
                 }
-                override fun onCancelled(error: DatabaseError) {
-                }
-            })
+            }
+        }
     }
 }
