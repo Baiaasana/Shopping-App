ae1a0b8ba7630120bfe614664d974109c3a15252
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-11-06 20:25:56 +0400

    pop up toss... ra bagi ipova am debilma

--- app/src/main/java/com/bendg/bg/presenter/ui/activity/MainActivity.kt
+++ app/src/main/java/com/bendg/bg/presenter/ui/activity/MainActivity.kt
@@ -2,24 +2,13 @@ package com.bendg.bg.presenter.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bendg.bg.R
-import com.bendg.bg.common.Constants
 import com.bendg.bg.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -32,28 +21,8 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment?
-        val navView = binding.bottomNavigationView
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.ordersFragment,
-                R.id.favoritesFragment,
-                R.id.profileFragment
-            )
-        )
-
-        if (navHostFragment != null) {
-            val navController = navHostFragment.navController
-            // Setup NavigationUI
-            setupActionBarWithNavController(navController,
-                appBarConfiguration)
-            navView.setupWithNavController(navController)
-
-            findViewById<NavigationView>(R.id.nav_view)
-                .setupWithNavController(navController)
-        }
+        binding.bottomNavigationView.setupWithNavController(findNavController(R.id.nav_host_fragment_main))
+        binding.navView.setupWithNavController(findNavController(R.id.nav_host_fragment_main))
 
         binding.logOutView.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
